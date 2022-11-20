aa3a6633a61d7b6bd38032ca5e0bc41f67cc2980
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2022-11-20 17:25:28 +0700

    chore: Re-base SCSS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store'
+import Styles from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <Styles>
+      <App />
+    </Styles>
   </Provider>
 );
