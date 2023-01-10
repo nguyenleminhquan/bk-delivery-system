dab8eb2e76d32f980f71e58af3d3f1f8ee379441
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2023-01-10 23:54:34 +0700

    feat: Add logout to sidebar

--- frontend/src/pages/Sender/index.js
+++ frontend/src/pages/Sender/index.js
@@ -1,7 +1,6 @@
-import Sidebar from 'components/Sidebar';
 import { clearStore, testJWT } from 'features/user/userSlice';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Profile from 'pages/Profile';
+import {BsSearch} from 'react-icons/bs'
+import {BiPencil} from 'react-icons/bi'
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import styles from './Sender.module.scss'
@@ -14,8 +13,50 @@ function Sender() {
 	}, [])
 
 	return (
-		<div className='wrapper'>
-			
+		<div className={styles.wrapper}>
+			{/* Header bar */}
+			<div className='d-flex'>
+				<div className={styles.searchBar}>
+					<BsSearch />
+					<input type="text" placeholder='Nhập mã đơn hàng' className='ms-5' />
+				</div>
+				<button className='btn fs-4'>
+					<BiPencil className='me-3'/> Tạo đơn hàng
+				</button>
+			</div>
+
+			{/* General */}
+			<div>
+				<h1 className='pt-5 pb-3'>Tổng quan</h1>
+				{/* Đã lấy hàng filter */}
+				<div className="filter d-flex align-items-center">
+					<div className={`${styles.orderFilter} ${styles.orderFilterActive}`}>
+						<p className='font-weight-bold fs-1'>2</p>
+						<p>Đã lấy hàng</p>
+					</div>
+
+					<div className={styles.orderFilter}>
+						<p className='font-weight-bold fs-1'>1</p>
+						<p>Chưa lấy hàng</p>
+					</div>
+
+				</div>
+			</div>
+
+			{/* Table */}
+			<div>
+				<ul className={styles.orderSubFilter} role="button">
+					<li className={styles.active}>Đang xử lí</li>
+					<li>Đang giao</li>
+					<li>Chờ xác nhận lại</li>
+					<li>Trả hàng</li>
+				</ul>
+
+				{/* Order block */}
+				<div>
+					
+				</div>
+			</div>
 		</div>
 	)
 }
