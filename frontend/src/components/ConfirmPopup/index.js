2868238b3e1584757c726cd6b9835598b4f56b2c
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2023-01-26 15:42:48 +0700

    feat: create base popup component

---
+++ frontend/src/components/ConfirmPopup/index.js
@@ -0,0 +1,35 @@
+import styles from './ConfirmPopup.module.scss'
+
+function ConfirmPopup(props) {
+    // props contains: Title, content, OK event function
+    // How to use this component?
+    /**
+        E.g:
+        {isShown && (
+            <ConfirmPopup 
+                title="Cancel this order"
+                content="Do you want to cancel this order?"
+                actionNo={() => setIsShown(false)}
+                actionYes={() => onDelete()}
+            />
+        )} 
+    */ 
+    return (
+        <div className={styles.modal}>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <span>{props.title}</span>
+                </div>
+                <div className={styles.body}>
+                    <span>{props.content}</span>
+                </div>
+                <div className={styles.footer}>
+                    <button type="button" onClick={props.actionNo}>Cancel</button>
+                    <button type="button" className="btn ms-5" onClick={props.actionYes}>OK</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ConfirmPopup
