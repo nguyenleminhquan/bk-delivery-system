aa75f169d7bcaa8ad24e5c55dc7ac5b871be4f32
Hien24022001 <minhhien24022001@gmail.com>
2023-03-07 22:54:31 +0700

    feat: Implement minimal dashboard

--- frontend/src/pages/Roles/Admin/AdminHome/index.js
+++ frontend/src/pages/Roles/Admin/AdminHome/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import styles from './Admin.module.scss'
+import { FaMoneyBillAlt, FaShoppingBag, FaUserPlus } from 'react-icons/fa';
+import './index.scss'
 
 function AdminHome() {
   return (
-    <div className={styles.wrapper}>Admin</div>
+    <div className='admin-dashboard'>
+      <h2 className='pb-3 fs-3'>Thống kê</h2>
+      <div className='widget'>
+        <div className='widget-item blue-color'>
+          <p className='title'>Tài khoản mới</p>
+          <div className='d-flex justify-content-between align-items-center'>
+            <p className='num'>100</p>
+            <FaUserPlus className='icon' />
+          </div>
+        </div>
+        <div className='widget-item cyan-color'>
+          <p className='title'>Tổng đơn hàng</p>
+          <div className='d-flex justify-content-between align-items-center'>
+            <p className='num'>250</p>
+            <FaShoppingBag className='icon' />
+          </div>
+        </div>
+        <div className='widget-item orange-color'>
+          <p className='title'>Tổng doanh thu</p>
+          <div className='d-flex justify-content-between align-items-center'>
+            <p className='num'>1,45M</p>
+            <FaMoneyBillAlt className='icon' />
+          </div>
+        </div>
+        <div className='widget-item green-color'>
+          <p className='title'>Nhân viên mới</p>
+          <div className='d-flex justify-content-between align-items-center'>
+            <p className='num'>100</p>
+            <FaUserPlus className='icon' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
