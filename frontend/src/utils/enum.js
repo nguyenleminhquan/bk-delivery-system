d9c70a95bc6b9adac3c171634f3dfce482bb61b2
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2023-03-23 22:36:27 +0700

    feat: Integrated google map api for searching address

--- frontend/src/utils/enum.js
+++ frontend/src/utils/enum.js
@@ -1,9 +1,11 @@
 export const OrderStatus  = {
-    PROCESSING: 'processing',
-    DELIVERY: 'delivery',
-    RESEND_WAITING: 'resend_waiting',
-    SUCCESS: 'success',
-    RETURNING: 'returning',
-    RETURNING_SUCCESS: 'return_success',
-    DAMAGED: 'damaged'
+    WAITING: 'delivery',
+    ACCEPTED: 'accepted',
+    PICKED: 'picked',
+    ARRIVED_SEND_STOCK: 'arrived_send_stock',
+    ON_VEHICLE: 'on_vehicle',
+    COMING_DEST_STOCK: 'coming_dest_stock',
+    ARRIVED_DEST_STOCK: 'arrived_dest_stock',
+    DELIVERING: 'delivering',
+    SUCCESS: 'success'
 }
