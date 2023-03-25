b59bf2f1301d9c3e6fe7c512bd75fb06a56bb0a6
Hien24022001 <minhhien24022001@gmail.com>
2023-03-25 22:45:53 +0700

    chore: add filter by date

--- frontend/src/components/DatePickerComp/index.js
+++ frontend/src/components/DatePickerComp/index.js
@@ -2,14 +2,26 @@ import React, { useState } from 'react'
 import ReactDatePicker from 'react-datepicker'
 
 import "react-datepicker/dist/react-datepicker.css";
+import { toast } from 'react-toastify';
 import './index.scss'
 
-function DatePickerComp() {
-  const [startDate, setStartDate] = useState(new Date());
+function DatePickerComp({ handleSearch }) {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   const [option, setOption] = useState('byDate')
 
   const handleChange = (e) => {
     setOption(e.target.value);
+    setStartDate(null)
+    setEndDate(null)
+  }
+
+  const handleSubmit = () => {
+    if (startDate && endDate) {
+      handleSearch(startDate, endDate) 
+      return
+    }
+    toast.warning('Chưa chọn thời gian')
   }
 
   return (
@@ -25,23 +37,56 @@ function DatePickerComp() {
         {
           option === 'byDate' 
           ? <ReactDatePicker 
+              placeholderText='Chọn một ngày'
               selected={startDate}
-              onChange={(date) => setStartDate(date)}
+              onChange={(date) => {
+                setStartDate(date)
+                setEndDate(date)
+              }
+            }
             />
           : (option === 'byMonth'
           ? <ReactDatePicker 
+              placeholderText='Chọn một tháng'
               selected={startDate}
-              onChange={(date) => setStartDate(date)}
+              onChange={(date) => {
+                setStartDate(date.setDate(1))
+                setEndDate(date.setMonth(date.getMonth() + 1).setDate(date.getDate() - 1))
+              }}
               dateFormat="MM/Y"
               showMonthYearPicker
             />
-            : <ReactDatePicker 
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                show
-              />)
+            : <div className='date-range'>
+                <div>
+                  <span>Từ</span>
+                  <ReactDatePicker
+                    placeholderText='Chọn ngày bắt đầu'
+                    dateFormat="dd/MM/Y"
+                    selected={startDate}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    onChange={date => setStartDate(date)}
+                  />
+                </div>
+                <div>
+                  <span>Đến</span>
+                  <ReactDatePicker
+                    placeholderText='Chọn ngày kết thúc'
+                    dateFormat="dd/MM/Y"
+                    selected={endDate}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    onChange={date => setEndDate(date)}
+                  />
+                </div>
+            </div>
+          )    
         }
       </div>
+      <button className='btn btn-medium' onClick={handleSubmit}>Tìm kiếm</button>
          
     </div>
   )
--- frontend/src/components/StockerHeader/index.js
+++ frontend/src/components/StockerHeader/index.js
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { BiExport, BiPencil } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 import './index.scss'
 import * as XLSX from 'xlsx'
-import GeneralConfirm from 'components/GeneralConfirm'
-import DatePicker from 'components/DatePickerComp'
 
 const data = [
   {
@@ -31,7 +29,6 @@ const data = [
 ]
 
 function StockerHeader({ title, openImportPopup }) {
-  const [toggleExportExcel, setToggleExportExcel] = useState(false);
 
   const handleExport = () => {
     let wb = XLSX.utils.book_new();
@@ -45,7 +42,7 @@ function StockerHeader({ title, openImportPopup }) {
     <div className='stocker-header d-flex justify-content-between'>
         <h2 className='pb-3 fs-3'> { title } </h2>
         <div className='btns'>
-            <button className='btn fs-6' onClick={() => setToggleExportExcel(true)}>
+            <button className='btn fs-6' onClick={handleExport}>
               <BiExport className='me-3' /> Xuất excel
             </button>
             <Link className='btn fs-6' to="/export-order">
@@ -55,21 +52,6 @@ function StockerHeader({ title, openImportPopup }) {
               <BiPencil className='me-3' /> Nhập kho
             </button>
         </div>
-        {
-          toggleExportExcel &&
-          <GeneralConfirm
-            title="Xuất báo cáo ra file excel" 
-            onCancel={() => setToggleExportExcel(false)}
-            cancelText="Hủy bỏ"
-            showConfirmButton={true}
-            confirmText="Xuất file"
-            onConfirm={() => {
-              handleExport();
-              setToggleExportExcel(false)
-            }}
-            message={<DatePicker />}
-          />
-        }
     </div>
   )
 }
