510436aac0cb605ad0ab94efb25859734f4ce210
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2023-06-20 23:42:17 +0700

    fix: handle import order

--- frontend/src/components/ImportOrder/index.js
+++ frontend/src/components/ImportOrder/index.js
@@ -1,15 +1,22 @@
 import FormInput from 'components/FormInput'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai';
 import { FaSearch } from 'react-icons/fa';
 import './index.scss'
+import { useDispatch, useSelector } from 'react-redux';
+import { importOrderToStock } from 'features/stock/stockSlice';
+import { getOrderById } from 'features/user/orderSlice';
 
 function ImportOrder() {
+	const { user } = useSelector(state => state.user);
+	const { order } = useSelector(state => state.order);
+	const dispatch = useDispatch();
 	const [orderId, setOrderId] = useState('');
 	const [orderInfo, setOrderInfo] = useState('');
 
 	const handleSearch = (e) => {
 		e.preventDefault();
+		dispatch(getOrderById(orderId));
 		setOrderInfo({
 			id: '123123123',
 			name: 'Phước Tài',
@@ -18,9 +25,22 @@ function ImportOrder() {
 	}
 
 	const handleImport = () => {
+		const payload = {
+			order_id: orderId,
+			stock_id: '',
+			stocker_id: user?._id,
+		}
+		dispatch(importOrderToStock(payload));
 		setOrderId('');
 		setOrderInfo('');
 	}
+
+	useEffect(() => {
+		if (order?._id) {
+			console.log(order);
+			setOrderInfo({ id: order._id, name: order?.name, price: order?.price })
+		}
+	}, [order])
 
 	return (
 		<div className='import-order'>
