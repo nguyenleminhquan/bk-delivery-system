a83a688558d8d2c39d95ae2fea8352249827717b
phuoctai1644 <taiphuoctran123@gmail.com>
2023-07-30 15:59:38 +0700

    fix: bugs all-checked in case avail-order empty

--- frontend/src/pages/LoadOrderToTruck/index.js
+++ frontend/src/pages/LoadOrderToTruck/index.js
@@ -155,7 +155,6 @@ function LoadOrderToTruck() {
     }
 
     const handleOpenTruckOrders = () => {
-        console.log(truckLoad);
         setOpenPopup(true);
     }
 
@@ -174,7 +173,7 @@ function LoadOrderToTruck() {
     }
 
     const isAllChecked = () => {
-        return truckOrders?.every(order => order.checked);
+        return truckOrders.length > 0 && truckOrders?.every(order => order.checked);
     }
 
     const handleSortCol = () => {
